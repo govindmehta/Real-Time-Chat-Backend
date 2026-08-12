daf0bd29f34f210eb8aa12a3a470bfe999d7ca76
Govind Mehta <govindmehta.gov@gmail.com>
2026-08-12 11:02:34 +0530

    added io connection and added test client

---
+++ client_test/client.test.ts
@@ -0,0 +1,13 @@
+import { io } from "socket.io-client";
+
+console.log("Starting client...");
+
+const socket = io("http://localhost:5000");
+
+socket.on("connect", () => {
+    console.log("Connected:" + socket.id);
+});
+
+socket.on("connect_error", (error) => {
+    console.log("Connection error:", error.message);
+});
--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
+import { Socket } from "socket.io";
 import app from "./app";
 import { PORT } from "./config/env.config";
-import { server } from "./socket/socket";
+import { io, server } from "./socket/socket";
 
 
 const startServer = async () => {
     //server already have app and socket.io instance
     server.listen(PORT, () => {
         console.log(`Socket server is running on port ${PORT}`);
+    })
+    io.on("connection", (socket: Socket) => {
+
+        console.log(`New client connected: ${socket.id}`);
+
+        socket.on("disconnect", ()=>{
+            console.log(`Client disconnected: ${socket.id}`);
+        })
     })
 };
 
